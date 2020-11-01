f6c0ba2b395d25472e99cff97213f232b2988e4e
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-10-31 22:07:51 -0700

    Add files via upload

---
+++ add-repo.sh
@@ -0,0 +1,6 @@
+# Reference: https://flight-manual.atom.io/getting-started/sections/installing-atom/
+
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+
